f0ac59fd1d70dd15a6e512497e1a2f3a3014fa87
PlonkaKonrad <konrad66a4@gmail.com>
2021-02-14 22:54:31 +0100

    add chart draw function

--- script.js
+++ script.js
@@ -1,83 +1,85 @@
 
-
-
-let key = 'MAFIRWJKPWJ3T0GS';
-let querry = 'https://www.alphavantage.co/query?';
-let example = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=60min&apikey=MAFIRWJKPWJ3T0GS"
-
-
-
-
-
-
 function fetchData() {
 
-   
-    fetch(example)
+
+
+    let example2 = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=TSLA&apikey=MAFIRWJKPWJ3T0GS"
+
+
+
+    fetch(example2)
        .then(response =>{
            return response.json();
        }).then(data =>{
-        console.log(data);
+        //console.log(data);
 
+        let timeEntries = Object.entries(data['Weekly Time Series']);
+    
 
-
+        let daysArr = [];
+        let openPriceArr =[]
+        let highPriceArr =[]
+        let lowPriceArr =[]
+        let closePriceArr =[]
         
 
-         let information = data['Meta Data']['1. Information'];
-         let symbol = data['Meta Data']['2. Symbol'];
-         let interval = data['Meta Data']['4. Interval'];
+        for(let i = 0 ; i < timeEntries.length ; i++){
 
 
-         let timeEntries = Object.entries(data['Time Series (60min)']);
-         
-         let timeEntry = Object.entries(timeEntries[0]); // 0 - tu ma być płynne, nie na sztywno (po kolei wszystkie daty)
-         
-         let timeArr = Object.entries(timeEntry[0]); 
-
-         let time = timeArr[1][1]; // czas aktualizacji ceny
-
-         let priceArr = Object.entries(timeEntry[1]);
-
-         let pricesArr = Object.entries(priceArr[1][1]); // zwrócenie tablicy wszystkich cen
-
-         let openPrice = pricesArr[0][1];  //cena otwarcia
-         let highPrice = pricesArr[1][1];  //najwyższa cena w przedziale czasowym
-         let lowPrice  = pricesArr[2][1];  // najniższa cena w przedziale czasowym 
-         let closePrice = pricesArr[3][1]; // cena zamknięcia
-         let volume = pricesArr[4][1];   // wolumen
+            let day = timeEntries[i][0];
+            let prices = Object.entries(timeEntries[i][1]);
 
 
+            
+            let openPrice = prices[0][1];
+            let highPrice = prices[0][2];
+            let lowPrice = prices[0][3];
+            let closePrice = prices[0][4];
+            
+
+            daysArr.push(day);
+            openPriceArr.push(openPrice)
+            highPriceArr.push(highPrice)
+            lowPriceArr.push(lowPrice)
+            closePriceArr.push(closePrice)
 
 
-         
-   //zmapować wszyskie czasy wyciągnąć z nich ceny zamknięcia następnie wrzucic tablice z cenami zamknięcia jako
-   //data do  funckcji rysującej wykres
+    
 
+        }
 
-   //wyciągnąć tak samo daty  i wrzucić jako tablice do  tablicy labels w funcji drawChart
-
-         
-
-
-   
-   
-   
+        let daysArrReverse = daysArr.reverse();
+        let openPriceArrReverse = openPriceArr.reverse();
+        let highPriceArrReverse = highPriceArr.reverse();
+        let lowPriceArrReverse = lowPriceArr.reverse();
+        let closePriceArrReverse = closePriceArr.reverse();
+        
+        
+        
+            console.log(daysArrReverse);
+            drawChart(openPriceArrReverse, daysArrReverse);
+        
+        
+        
+        
        })
    }
 
 
 
+
+
    //////////////////////////////////////////////////////////////// CHART FUNCTION
 
-function drawChart() {
-var ctx = document.getElementById('chart').getContext('2d');
-var myChart = new Chart(ctx, {
+function drawChart(data, date) {
+const ctx = document.getElementById('chart').getContext('2d');
+const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: date,
         datasets: [{
             label: 'Price',
-            data: [12, 30, 3, 5, 2, 3],
+            data: data,
             backgroundColor: [
                 'rgba(84, 255, 187, 0.2)'
              
@@ -104,9 +106,6 @@ var myChart = new Chart(ctx, {
 
 
 
-
-
-
 ///////////////////////////////////////////////////////////////// GLOW BUTTON
 
 $('.btn').on("click", function(){
@@ -115,7 +114,8 @@ $('.btn').on("click", function(){
         $('.btn').removeClass('btn-active')
         $(this).toggleClass('btn-active');
 
-        drawChart();
+       
+        fetchData();
     
         
         
